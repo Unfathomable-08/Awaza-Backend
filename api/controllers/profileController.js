5fb021f468b25ad2cb51b05718a5527f8c3905d0
Muhammad <muhammad124711@gmail.com>
2026-03-11 23:41:14 +0500

    get-proifle

--- api/controllers/profileController.js
+++ api/controllers/profileController.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 // Get profile data
 const getProfile = async (req, res) => {
   try {
-    const user = await User.findById(req.user.id);
+    const user = await User.findOne({ username: req.query.username });
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
